6218f8c8e46f4888e640fee387528f177ceffc51
Tantalor93 <Obenky@gmail.com>
2018-09-08 22:24:40 +0200

    initial commit

---
+++ src/main/java/com/github/tantalor93/rest/MessageController.java
@@ -0,0 +1,38 @@
+package com.github.tantalor93.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class MessageController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(final KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @GetMapping(
+            value = "/ping"
+    )
+    public ResponseEntity<String> ping() throws ExecutionException, InterruptedException {
+
+        long now = System.currentTimeMillis();
+
+        ListenableFuture<SendResult<String, String>> test = kafkaTemplate.send(
+                "test",
+                "message-app" + now,
+                "benky" + (now % 99)
+        );
+
+        SendResult<String, String> stringStringSendResult = test.get();
+
+        return ResponseEntity.ok(stringStringSendResult.toString());
+    }
+}
